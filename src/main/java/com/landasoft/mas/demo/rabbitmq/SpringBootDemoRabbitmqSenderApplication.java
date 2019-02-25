41cf99ce96b3ac08caa03e79a6cd65eaa136f6ab
wulinyun <lin.wu@landasoft.com>
2019-02-25 14:12:49 +0800

    更新

---
+++ src/main/java/com/landasoft/mas/demo/rabbitmq/SpringBootDemoRabbitmqSenderApplication.java
@@ -0,0 +1,13 @@
+package com.landasoft.mas.demo.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoRabbitmqSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoRabbitmqSenderApplication.class, args);
+	}
+
+}
